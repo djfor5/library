29d11f58217f6ad30d1786e5ec30b6a155945b43
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-05-14 22:21:22 +1000

    Display card with book details

--- main.js
+++ main.js
@@ -1,44 +1,71 @@
 // FORM
-const authorInput = document.querySelector('#author')
 const titleInput = document.querySelector('#title')
+const authorInput = document.querySelector('#author')
 const pagesInput = document.querySelector('#pages')
 const readYesInput = document.querySelector('#read-yes')
 const readNoInput = document.querySelector('#read-no')
 const saveBtn = document.querySelector('#save-book')
 
+
 // CARDS
-const library = document.querySelector('#library')
+const cardsSection = document.querySelector('#cards')
+
 
 saveBtn.addEventListener('click',(event)=>{
   event.preventDefault()
   const readInput = document.querySelector('input[name="read"]:checked')
-  addBookToLibrary(authorInput.value, titleInput.value, pagesInput.value, readInput.value)
+  addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value)
   
   // RESET FORM VALUES
-  authorInput.value = ""
   titleInput.value = ""
+  authorInput.value = ""
   pagesInput.value = ""
   readYesInput.checked = false
   readNoInput.checked = false
 })
 
+
 let myLibrary = [];
 
-function Book(author, title, pages, read) {
+
+function Book(title, author, pages, read) {
   if (!(this instanceof Book)) {
     // console.warn('The "new" keyword must be used to call Book')
-    return new Book(author, title, pages, read)
+    return new Book(title, author, pages, read)
   }
 
-  this.author = author
   this.title = title
+  this.author = author
   this.pages = pages
   this.read = read
 }
 
-function addBookToLibrary(author, title, pages, read) {
-  const newBook = new Book(author, title, pages, read)
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read)
   myLibrary.push(newBook)
-  library.textContent = myLibrary
-  console.log(myLibrary)
+
+  // CREATE NEW CARD
+  const card = document.createElement('p')
+
+  // ADD P TAGS
+  const pTitle = document.createElement('p')
+  const pAuthor = document.createElement('p')
+  const pPages = document.createElement('p')
+  const pRead = document.createElement('p')
+  
+  // ADD TEXT
+  pTitle.textContent = title
+  pAuthor.textContent = author
+  pPages.textContent = pages
+  pRead.textContent = read
+  
+  // APPEND P TAGS TO CARD
+  card.appendChild(pTitle)
+  card.appendChild(pAuthor)
+  card.appendChild(pPages)
+  card.appendChild(pRead)
+  
+  // APPEND CARD TO BODY
+  cardsSection.appendChild(card)
 }
