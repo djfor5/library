86fb3ba329af7c430269bd744a02c657ddf8d27f
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-05-21 22:34:47 +1000

    Add delete functionality for each book

--- main.js
+++ main.js
@@ -13,31 +13,43 @@ const saveBtn = document.querySelector('#save-book')
 
 
 // CARDS
-const cardsSection = document.querySelector('#cards')
+let cardsSection = document.querySelector('#cards')
+
+
+let myLibrary = [];
+let bookId = 0
 
 
 newBtn.addEventListener('click', () => {
+  console.log('New book button clicked')
   form.removeAttribute('hidden')
 })
 
 
 saveBtn.addEventListener('click', (event) => {
   event.preventDefault()
-  const readInput = document.querySelector('input[name="read"]:checked')
-  addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value)
+  console.log('Save book button clicked')
   
-  // RESET FORM VALUES
-  titleInput.value = ""
-  authorInput.value = ""
-  pagesInput.value = ""
-  readYesInput.checked = false
-  readNoInput.checked = false
+  // get values from input fields
+  const title = titleInput.value
+  const author = authorInput.value
+  const pages = pagesInput.value
+  const readInput = document.querySelector('input[name="read"]:checked')
+  
+  if (!title || !author || !read) {
+    return console.warn('Missing inputs')
+  }
+  const read = readInput.value
+
+
+  // create new book object and add to myLibrary array
+  addBookToLibrary(title, author, pages, read)
+  renderBooks(myLibrary)
+
+  resetFormValues()
 
   form.setAttribute('hidden', 'true')
 })
-
-
-let myLibrary = [];
 
 
 function Book(title, author, pages, read) {
@@ -55,30 +67,69 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read)
+  newBook.bookId = bookId
+  bookId++
   myLibrary.push(newBook)
 
-  // CREATE NEW CARD
-  const cardDiv = document.createElement('div')
-  cardDiv.classList.add('card')
+  console.log('New book added to myLibrary array')
+  console.log(myLibrary)
+}
 
-  // ADD P TAGS
-  const pTitle = document.createElement('p')
-  const pAuthor = document.createElement('p')
-  const pPages = document.createElement('p')
-  const pRead = document.createElement('p')
-  
-  // ADD TEXT
-  pTitle.textContent = `Title: ${title}`
-  pAuthor.textContent = `Author: ${author}`
-  pPages.textContent = `Pages: ${pages}`
-  pRead.textContent = `Read: ${read}`
-  
-  // APPEND P TAGS TO CARD
-  cardDiv.appendChild(pTitle)
-  cardDiv.appendChild(pAuthor)
-  cardDiv.appendChild(pPages)
-  cardDiv.appendChild(pRead)
-  
-  // APPEND CARD TO BODY
-  cardsSection.appendChild(cardDiv)
+
+function renderBooks(booksArr) {
+  cardsSection.innerHTML = "" // delete all cards, card content, and corresponding event listeners
+
+  booksArr.forEach((bookObj)=>{
+    // create card and info elements
+    const divCard = document.createElement('div')
+    const pTitle = document.createElement('p')
+    const pAuthor = document.createElement('p')
+    const pPages = document.createElement('p')
+    const pRead = document.createElement('p')
+    const btnDelete = document.createElement('button')
+
+    
+    // add attributes and text content to elements
+    divCard.classList.add('card')
+    pTitle.textContent = `Title: ${bookObj.title}`
+    pAuthor.textContent = `Author: ${bookObj.author}`
+    pPages.textContent = `Pages: ${bookObj.pages}`
+    pRead.textContent = `Read: ${bookObj.read}`
+    btnDelete.classList.add('delete')
+    btnDelete.dataset.bookId = bookObj.bookId
+    btnDelete.textContent = 'Delete book'
+
+    // append elements to divCard and divCard to cardsSection
+    divCard.appendChild(pTitle)
+    divCard.appendChild(pAuthor)
+    divCard.appendChild(pPages)
+    divCard.appendChild(pRead)
+    divCard.appendChild(btnDelete)
+    cardsSection.appendChild(divCard)
+  })
+  addDeleteBtns()
+}
+
+
+function resetFormValues() {
+  titleInput.value = ""
+  authorInput.value = ""
+  pagesInput.value = ""
+  readYesInput.checked = false
+  readNoInput.checked = false
+}
+
+
+function addDeleteBtns() {
+  const deleteBtns = document.querySelectorAll('.delete')
+  deleteBtns.forEach((deleteBtn)=>{
+    deleteBtn.addEventListener('click', (event)=>{
+      console.log('Delete button clicked')
+      const thisDeleteBtnId = Number(event.target.dataset.bookId)
+
+      myLibrary = myLibrary.filter(bookObj => bookObj.bookId !== thisDeleteBtnId)
+
+      renderBooks(myLibrary)
+    })     
+  })
 }
